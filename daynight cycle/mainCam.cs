3663595b6ffe91876cfabcccf244abdc89af670c
arrowhead422 <heroes618@hotmail.com>
2017-02-14 15:53:21 -0800

    daynight cycle

---
+++ daynight cycle/mainCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCam : MonoBehaviour {
+
+    public Transform targetPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void FixedUpdate()
+    {
+        transform.LookAt(targetPos.position);
+
+       // transform.LookAt(new Vector3(targetPos.position.x, 0f, targetPos.position.z));
+    }
+}
