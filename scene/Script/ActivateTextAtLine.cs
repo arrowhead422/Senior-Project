3dc2beeaa4186338de45cc1bb493308e34cba690
arrowhead422 <heroes618@hotmail.com>
2017-04-28 11:24:00 -0700

    new asset and new scene-graveyard

---
+++ scene/Script/ActivateTextAtLine.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    
+    public class ActivateTextAtLine : MonoBehaviour
+    {
+        public TextAsset theText;
+
+        public int starLine;
+        public int endLine;
+
+        public TextBoxManager theTextBox;
+
+        public bool requireButtonPress;
+        private bool waitforPress;
+
+        public bool destroyWhenActivated;
+
+        // Use this for initialization
+        void Start()
+        {
+            theTextBox = FindObjectOfType<TextBoxManager>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(waitforPress && Input.GetKeyDown(KeyCode.J))
+            {
+                theTextBox.ReloadScript(theText);
+                theTextBox.currentLine = starLine;
+                theTextBox.endAtLine = endLine;
+                theTextBox.EnableTextBox();
+
+                if (destroyWhenActivated)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        void OnTriggerEner3D(Collider other)
+        {
+            if(other.name == "player")
+            {
+                if (requireButtonPress)
+                {
+                    waitforPress = true;
+                    return;
+                }
+                theTextBox.ReloadScript(theText);
+                theTextBox.currentLine = starLine;
+                theTextBox.endAtLine = endLine;
+                theTextBox.EnableTextBox();
+
+                if (destroyWhenActivated)
+                {
+                    Destroy(gameObject);
+                }
+
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if(other.name == "player")
+            {
+                waitforPress = false;
+            }
+        }
+    }
+}
---
+++ scene/Script/TextBoxManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    public class TextBoxManager : MonoBehaviour
+    {
+
+        public GameObject textBox;
+
+        public Text theText;
+
+        public TextAsset textFile;
+        public string[] textLines;
+
+        public int currentLine;
+        public int endAtLine;
+
+        public ThirdPersonCharacter player;
+
+
+        public bool isActive;
+
+        public bool stopPlayerMovement;
+
+        private bool isTyping = false;
+        private bool cancelTyping = false;
+
+        public float typeSpeed;
+
+        // Use this for initialization
+        void Start()
+        {
+
+            player = FindObjectOfType<ThirdPersonCharacter>();
+
+            if (textFile != null)
+            {
+                textLines = (textFile.text.Split('\n'));
+            }
+
+            if (endAtLine == 0)
+            {
+                endAtLine = textLines.Length - 1;
+            }
+            if (isActive)
+            {
+                EnableTextBox();
+            }
+            else
+            {
+                DisableTextBox();
+            }
+        }
+
+        void Update()
+        {
+            if (!isActive)
+            {
+                return;
+            }
+
+            //theText.text = textLines[currentLine];
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (!isTyping)
+                {
+
+                    currentLine += 1;
+
+                    if (currentLine > endAtLine)
+                    {
+                        DisableTextBox(); //the universal class :)
+                    }
+                    else
+                    {
+                        StartCoroutine(TextScroll(textLines[currentLine]));
+                    }
+                }
+                else if (isTyping && !cancelTyping)
+                {
+                    cancelTyping = true;
+                }
+            }
+         
+        }
+
+        private IEnumerator TextScroll (string lineOfText)
+        {
+            int letter = 0;
+            theText.text = "";
+            isTyping = true;
+            cancelTyping = false;
+            while (isTyping && !cancelTyping && (letter < lineOfText.Length - 1))
+            {
+                theText.text += lineOfText[letter];
+                letter += 1;
+                    yield return new WaitForSeconds(typeSpeed);
+            }
+            theText.text = lineOfText;
+            isTyping = false;
+            cancelTyping = false;
+       }
+
+        public void EnableTextBox()
+        {
+            textBox.SetActive(true);
+            isActive = true;
+
+            if (stopPlayerMovement)
+            {
+                player.canMove = false;
+            }
+
+            StartCoroutine(TextScroll(textLines[currentLine]));
+        }
+
+        public void DisableTextBox()
+        {
+            textBox.SetActive(false);
+            isActive = false;
+            player.canMove = true;
+        }
+
+        public void ReloadScript(TextAsset theText)
+        {
+            if(theText != null)
+            {
+                textLines = new string[1];
+                textLines = (theText.text.Split('\n'));
+            }
+        }
+
+
+    }
+}
